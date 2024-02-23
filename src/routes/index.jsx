93708a9ce0a3f60505d008ea30b7d3356086f72d
José Eduardo Antúnez Aguilar <jose.cfeteit@gmail.com>
2024-02-23 16:17:48 -0600

    i'm a dummy

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import UsuariosPage from '../pages/usuarios'
 import AlbumesPage from '../pages/albumes'
+import FotosPage from '../pages/fotos'
 
 export default function Router () {
   return (
@@ -9,6 +10,7 @@ export default function Router () {
         <Route path="/" element={<Navigate to="/usuarios" replace />} />
         <Route path="/usuarios" element={<UsuariosPage />} />
         <Route path='/albumes/:userId' element={<AlbumesPage/>}/>
+        <Route path='/fotos/:id' element={<FotosPage/>}/>
       </Routes>
     </BrowserRouter>
   )
